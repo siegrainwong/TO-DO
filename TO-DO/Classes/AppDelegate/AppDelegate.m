018b5d84b76d3e4eab417c421c65e8228c3c3f17
SeanWong933 <siegrainwong@gmail.com>
2016-11-07 20:03:14 +0800

    增加切换rootviewcontroller时的过渡特效

--- TO-DO/Classes/AppDelegate/AppDelegate.m
+++ TO-DO/Classes/AppDelegate/AppDelegate.m
@@ -32,36 +32,33 @@ AppDelegate ()
 
 @implementation AppDelegate
 #pragma mark - application delegate
-- (BOOL)application:(UIApplication*)application
-didFinishLaunchingWithOptions:(NSDictionary*)launchOptions
-{
-    [self setup];
 
+- (BOOL)          application:(UIApplication *)application
+didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [self setup];
+    
     return YES;
 }
-- (void)applicationWillResignActive:(UIApplication*)application
-{
+
+- (void)applicationWillResignActive:(UIApplication *)application {
 }
 
-- (void)applicationDidEnterBackground:(UIApplication*)application
-{
+- (void)applicationDidEnterBackground:(UIApplication *)application {
 }
 
-- (void)applicationWillEnterForeground:(UIApplication*)application
-{
+- (void)applicationWillEnterForeground:(UIApplication *)application {
 }
 
-- (void)applicationDidBecomeActive:(UIApplication*)application
-{
+- (void)applicationDidBecomeActive:(UIApplication *)application {
 }
 
-- (void)applicationWillTerminate:(UIApplication*)application
-{
+- (void)applicationWillTerminate:(UIApplication *)application {
     [MagicalRecord cleanUp];
 }
+
 #pragma mark - initial
-- (void)setup
-{
+
+- (void)setup {
     [self setupNetworkEye];
     [self setupDDLog];
     [self setupMagicRecord];
@@ -71,27 +68,28 @@ didFinishLaunchingWithOptions:(NSDictionary*)launchOptions
     [self setupDrawerViewController];
     [self applicationDocumentsDirectory];
     //    [self insertTestTodoToLC];
-
+    
     [UIApplication sharedApplication].statusBarStyle = UIStatusBarStyleLightContent;
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
-
+    
     // validate user's login state
     if (_lcUser) {
         DDLogInfo(@"当前用户：%@", _lcUser.username);
-        HomeViewController* homeViewController = [HomeViewController new];
+        HomeViewController *homeViewController = [HomeViewController new];
         [self switchRootViewController:homeViewController isNavigation:YES];
     } else {
         [self switchRootViewController:[LoginViewController new] isNavigation:NO];
     }
-
+    
     [self.window makeKeyAndVisible];
 }
-- (void)setupUser
-{
+
+- (void)setupUser {
     _lcUser = [LCUser currentUser];
     if (_lcUser) _cdUser = [CDUser userWithLCUser:_lcUser];
 }
+
 - (void)setupNetworkEye {
 #if defined(DEBUG)|| defined(_DEBUG)
     [NEHTTPEye setEnabled:YES];
@@ -99,61 +97,61 @@ didFinishLaunchingWithOptions:(NSDictionary*)launchOptions
     [NEHTTPEye setEnabled:NO];
 #endif
 }
-- (void)setupLeanCloud
-{
+
+- (void)setupLeanCloud {
     // setup leanCloud with appId and key
     [AVOSCloud setApplicationId:kLeanCloudAppID clientKey:kLeanCloudAppKey];
-
+    
     // register subclasses
     [LCSyncRecord registerSubclass];
     [LCSync registerSubclass];
     [LCUser registerSubclass];
     [LCTodo registerSubclass];
 }
-- (void)setupDrawerViewController
-{
+
+- (void)setupDrawerViewController {
     _drawerViewController = [JVFloatingDrawerViewController new];
     _drawerViewController.leftDrawerWidth = kScreenHeight * 0.3;
-    JVFloatingDrawerSpringAnimator* animator = [JVFloatingDrawerSpringAnimator new];
+    JVFloatingDrawerSpringAnimator *animator = [JVFloatingDrawerSpringAnimator new];
     animator.animationDuration = 0.5;
     animator.initialSpringVelocity = 2;
     animator.springDamping = 0.8;
     _drawerViewController.animator = animator;
-
+    
     _drawerViewController.leftViewController = [DrawerTableViewController new];
-
+    
     _drawerViewController.backgroundImage = [UIImage imageAtResourcePath:@"drawerbg"];
 }
-- (void)setupDDLog
-{
+
+- (void)setupDDLog {
     [DDLog addLogger:[DDTTYLogger sharedInstance]];
     [[DDTTYLogger sharedInstance] setColorsEnabled:YES];  //允许颜色
     [[DDTTYLogger sharedInstance] setForegroundColor:[UIColor blueColor] backgroundColor:nil forFlag:DDLogFlagInfo];
-
-    DDFileLogger* fileLogger = [[DDFileLogger alloc] init];  // File Logger
+    
+    DDFileLogger *fileLogger = [[DDFileLogger alloc] init];  // File Logger
     fileLogger.rollingFrequency = 60 * 60 * 24;              // 24 hour rolling
     fileLogger.logFileManager.maximumNumberOfLogFiles = 7;   //最长保留一周
     [DDLog addLogger:fileLogger];
 }
-- (void)setupMagicRecord
-{
+
+- (void)setupMagicRecord {
     [MagicalRecord setupAutoMigratingCoreDataStack];
 }
-- (void)setupReachability
-{
+
+- (void)setupReachability {
     _reachability = [RealReachability sharedInstance];
     [_reachability startNotifier];
 }
-- (void)truncateLocalData
-{
+
+- (void)truncateLocalData {
     [CDUser MR_truncateAll];
     [CDTodo MR_truncateAll];
 }
-- (void)insertTestTodoToLC
-{
-    NSMutableArray* array = [NSMutableArray array];
+
+- (void)insertTestTodoToLC {
+    NSMutableArray *array = [NSMutableArray array];
     for (int i = 0; i < 100; i++) {
-        LCTodo* todo = [LCTodo object];
+        LCTodo *todo = [LCTodo object];
         todo.title = [NSString stringWithFormat:@"Test data：%d", i];
         todo.sgDescription = [NSString stringWithFormat:@"this is a fucking description: %d", i];
         todo.deadline = [[NSDate date] dateByAddingTimeInterval:arc4random() % 70000];
@@ -163,46 +161,51 @@ didFinishLaunchingWithOptions:(NSDictionary*)launchOptions
         todo.status = TodoStatusNormal;
         todo.syncVersion = 0;
         todo.identifier = [[NSUUID UUID] UUIDString];
-
-        int random = (int)(arc4random() % 2500000 - 5000000);
+        
+        int random = (int) (arc4random() % 2500000 - 5000000);
         todo.localCreatedAt = [[NSDate date] dateByAddingTimeInterval:random];
         todo.localUpdatedAt = [todo.localCreatedAt copy];
-
+        
         [array addObject:todo];
     }
-
+    
     [LCTodo saveAll:[array copy]];
 }
-- (NSURL*)applicationDocumentsDirectory
-{
-    NSLog(@"%@", [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject]);
 
+- (NSURL *)applicationDocumentsDirectory {
+    NSLog(@"%@", [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject]);
+    
     return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
 }
+
 #pragma mark - switch root view controller
-- (void)switchRootViewController:(UIViewController*)viewController isNavigation:(BOOL)isNavigation
-{
+
+- (void)switchRootViewController:(UIViewController *)viewController isNavigation:(BOOL)isNavigation {
     if (isNavigation) {
-        JTNavigationController* navigationController = [[JTNavigationController alloc] initWithRootViewController:viewController];
+        JTNavigationController *navigationController = [[JTNavigationController alloc] initWithRootViewController:viewController];
         _drawerViewController.centerViewController = navigationController;
     }
-    UIViewController * preViewController = self.window.rootViewController;
+    UIView *snapShot = [self.window snapshotViewAfterScreenUpdates:true];
+    [viewController.view addSubview:snapShot];
+    
     self.window.rootViewController = isNavigation ? _drawerViewController : viewController;
-    [preViewController removeFromParentViewController];
-    preViewController = nil;
+    
+    [UIView animateWithDuration:.5 animations:^{snapShot.layer.opacity = 0;} completion:^(BOOL finished) {[snapShot removeFromSuperview];}];
 }
+
 #pragma mark - JVDrawer
-- (void)toggleDrawer:(id)sender animated:(BOOL)animated
-{
+
+- (void)toggleDrawer:(id)sender animated:(BOOL)animated {
     [_drawerViewController toggleDrawerWithSide:JVFloatingDrawerSideLeft animated:animated completion:nil];
 }
-- (void)setCenterViewController:(UIViewController*)viewController
-{
+
+- (void)setCenterViewController:(UIViewController *)viewController {
     _drawerViewController.centerViewController = viewController;
 }
+
 #pragma mark - global access helper
-+ (AppDelegate*)globalDelegate
-{
-    return (AppDelegate*)[UIApplication sharedApplication].delegate;
+
++ (AppDelegate *)globalDelegate {
+    return (AppDelegate *) [UIApplication sharedApplication].delegate;
 }
 @end
